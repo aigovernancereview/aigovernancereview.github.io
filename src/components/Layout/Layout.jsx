ad92ff5dbd7109fc22375f29231f2d338748f8d6
Oly Su <oly.su@qantashotels.com>
2020-04-25 00:19:11 +1000

    Add Container to all layouts. Removing Gatsby starter theme's existing styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -9,6 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
+import Container from '../Container';
 import Footer from '../Footer';
 import Header from '../Header';
 
@@ -26,16 +27,10 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <Footer />
-      </div>
+      <main>
+        <Container>{children}</Container>
+      </main>
+      <Footer />
     </>
   );
 };
