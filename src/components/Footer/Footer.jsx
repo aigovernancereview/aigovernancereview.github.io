22e40aa07dd83df7130f6b46995c9c41258defcf
Oly Su <oly.su@qantashotels.com>
2020-04-24 21:52:30 +1000

    Make footer its own component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    {`This site is maintained by `}
+    <a href="https://www.github.com/olyism">Oly Su</a>
+  </footer>
+);
+
+export default Footer;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -9,6 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
+import Footer from '../Footer';
 import Header from '../Header';
 import './Layout.css';
 
@@ -34,10 +35,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          {`This site is maintained by `}
-          <a href="https://www.github.com/olyism">Oly Su</a>
-        </footer>
+        <Footer />
       </div>
     </>
   );
