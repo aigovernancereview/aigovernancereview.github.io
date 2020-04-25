f6b391772e7a0310e9e14c197116857d691c0526
Oly Su <oly.su@qantashotels.com>
2020-04-25 20:26:40 +1000

    Added customer 404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
+import Container from '../../components/Container';
 import Layout from '../../components/Layout';
+import Section from '../../components/Section';
 import SEO from '../../components/SEO';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>Not found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <>
+    <SEO title="Page not found" />
+    <Layout>
+      <Section isWhite>
+        <Container>
+          <h1>Sorry, page not found</h1>
+          <p>The page you&apos;re trying to access doesn&apos;t exist.</p>
+          <p>
+            {`Need help? Please contact us at `}
+            <a href="mailto:globalaigovernance@gmail.com">globalaigovernance@gmail.com</a>
+            {` or `}
+            <Link to="/">return to homepage</Link>
+          </p>
+        </Container>
+      </Section>
+    </Layout>
+  </>
 );
 
 export default NotFoundPage;
