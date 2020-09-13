6f5b917a678444675f4775b2e330c710bb70f7b0
Oly Su <taiya1229@gmail.com>
2020-09-13 18:17:59 +1000

    Updates

--- component---src-pages-not-found-page-index-js-9147e62746df59d6b955.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7,8],{"1tS0":function(t,e,n){"use strict";n.r(e);var a=n("gSx0");e.default=a.default},gSx0:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),l=n("Zttt"),o=n("wtQ5");e.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"Not found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-not-found-page-index-js-9147e62746df59d6b955.js.map
